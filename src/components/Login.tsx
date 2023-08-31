68910bfffccbf9995a920ff686ec18e6a9f1a262
Mandytrajano90 <mandytrajano90@gmail.com>
2023-08-31 15:44:30 -0300

    First Requirement - Creating form on Login route

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,7 +1,61 @@
+import { useNavigate } from 'react-router-dom';
 import './Login.css';
+import { useState } from 'react';
+import { createUser } from '../services/userAPI';
+import Loading from './Loading';
 
 function Login() {
-  return <h1 className="Login">Login</h1>;
+  const navigating = useNavigate();
+  const [name, setName] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  function handleClick() {
+    setLoading(true);
+  }
+
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const { value } = e.target;
+    setName(value);
+  }
+
+  function showForm(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    setLoading(true);
+    createUser({ name }).then((userName) => {
+      setName('');
+
+      if (userName === 'OK') {
+        setLoading(false);
+        navigating('/search');
+      }
+    });
+  }
+
+  return (
+    <>
+      { loading && (<Loading />) }
+      <form className="login-form" onSubmit={ showForm }>
+        <div>
+          <label htmlFor="login-name-input">Nome</label>
+          <input
+            type="text"
+            name="login-name-input"
+            data-testid="login-name-input"
+            placeholder="Nome"
+            value={ name }
+            onChange={ handleChange }
+          />
+        </div>
+        <button
+          data-testid="login-submit-button"
+          onClick={ handleClick }
+          disabled={ name.length < 3 }
+        >
+          Entrar
+        </button>
+      </form>
+    </>
+  );
 }
 
 export default Login;
